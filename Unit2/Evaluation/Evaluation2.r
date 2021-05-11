3520f4955cfa6043a2a27b78592650422a51ed6e
gerardo <gerarerm@gmail.com>
2021-05-11 14:24:10 -0700

    Size budget mill added on grafics

--- Unit2/Evaluation/Evaluation2.r
+++ Unit2/Evaluation/Evaluation2.r
@@ -18,15 +18,15 @@ library(ggplot2)
  
 filter(ProyectData, Genre == "action")
 
-f <- ProyectData %>% filter(Genre %in% c( "action", "adventure", "comedy", "drama", "horror", "romance", "thriller"), Studio %in% c( "Buena Vista Studios", "Fox", "Paramount Pictures", "Sony", "Universal", "WB"))
+f <- ProyectData %>% filter(Genre %in% c( "action", "adventure", "animation",  "comedy", "drama"), Studio %in% c( "Buena Vista Studios", "Fox", "Paramount Pictures", "Sony", "Universal", "WB"))
 
 # geom_point()
 
 u <- ggplot(f, aes(x=Genre, y=Gross...US,
-                        color=Studio)) 
+                        color=Studio, size = Budget...mill.)) 
 u + geom_point() + geom_jitter() 
 
-
+?geom_boxplot
 # **Es necesario pulir codigo para el uso de boxplot y  BubgetMill**
 
 # u + geom_boxplot(size=1.2) + geom_jitter()
