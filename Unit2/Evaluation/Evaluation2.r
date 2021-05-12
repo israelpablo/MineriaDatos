433367c3303752f6c21a1f8e4657f280b9814208
Israel Lopez Pablo <ilopez@primewheel.com>
2021-05-12 00:14:08 -0700

    add the chart, add geo_point, geo_boxplot,theme,ggtitle,geo_jitter,xlab,ylab

--- Unit2/Evaluation/Evaluation2.r
+++ Unit2/Evaluation/Evaluation2.r
@@ -19,15 +19,22 @@ library(ggplot2)
 filter(ProyectData, Genre == "action")
 
 f <- ProyectData %>% filter(Genre %in% c( "action", "adventure", "animation",  "comedy", "drama"), Studio %in% c( "Buena Vista Studios", "Fox", "Paramount Pictures", "Sony", "Universal", "WB"))
+f
 
-# geom_point()
+ggplot(f)+  
+  xlab("Genre") +
+  ylab("Gross%US") +
+  ggtitle("Dosmestic Gross% by Genre")+
+ geom_point(aes(x=Genre,y= Gross...US,colour = Studio,size = Budget...mill.))+
+  geom_jitter(aes(Genre, Gross...US, colour = Studio), 
+              position = position_jitter(width = .15, height=-0.7),
+              size=2
+              )+
+  geom_boxplot(aes(x=Genre,y= Gross...US), 
+               width=0.5,alpha=0.5
+    )+
+  theme(axis.title.x = element_text(color = "Purple", size=15),
+        axis.title.y = element_text(color = "Purple", size=15),
+        plot.title = element_text(color = "Black",
+                                  size = 20,hjust = 0.5))
 
-u <- ggplot(f, aes(x=Genre, y=Gross...US,
-                        color=Studio, size = Budget...mill.)) 
-u + geom_point() + geom_jitter() 
-
-?geom_boxplot
-# **Es necesario pulir codigo para el uso de boxplot y  BubgetMill**
-
-# u + geom_boxplot(size=1.2) + geom_jitter()
-# u + geom_point(aes(x=BudgetMillions)) + xlab("Budget Millions $$$")
---
+++ Unit2/Evaluation/Readme.md
@@ -0,0 +1,24 @@
+
+
+
+<h1 align="center"> Instituto Tecnológico de Tijuana </h1>
+<h3 align="center"> Subdirección académica departamento de sistemas y computación.</h3>
+
+
+<h4 align="center"> Mineria de datos.</h4>
+
+
+<h4 align="center"> JOSE CHRISTIAN ROMERO HERNANDEZ.</h4>
+
+
+<h4 align="center">Unit 2:</h4>
+
+<h4 align="center">Evaluation 2</h4>
+
+<h4 align="center">  Data mining</h4>
+
+
+<h4 align="center"> Lopez Pablo Israel 17210585</h4>
+<h4 align="center"> Reyes Armenta Gerardo Antonio 17211147</h4>
+
+
