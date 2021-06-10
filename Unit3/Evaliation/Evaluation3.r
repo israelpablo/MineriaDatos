571c65194a679e4b140d9238e9c600ce12738c36
gerardo <gerarerm@gmail.com>
2021-06-09 17:33:17 -0700

    Charge file csv, split the sets and Scaling these

--- Unit3/Evaliation/Evaluation3.r
+++ Unit3/Evaliation/Evaluation3.r
@@ -11,3 +11,6 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
