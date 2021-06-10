84ff069a0fea0e46ad04085b38ffad6c254c919f
gerardo <gerarerm@gmail.com>
2021-06-09 17:29:27 -0700

    File Evaluation 3 was added

---
+++ Unit3/Evaliation/Evaluation3.r
@@ -0,0 +1,13 @@
+# Analysis of data visualization in the model of the Random Forest
+# Importing the dataset
+dataset = read.csv(file.choose())
+dataset = dataset[3:5]
+
+# Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
